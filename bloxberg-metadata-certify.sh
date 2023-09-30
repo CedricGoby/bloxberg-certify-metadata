ae9458144e99e272e1447e322998bd52f6db2162
Cédric Goby <cedric.goby@inrae.fr>
2023-09-30 16:23:41 +0200

    Commentaires

--- bloxberg-metadata-certify.sh
+++ bloxberg-metadata-certify.sh
@@ -3,37 +3,30 @@
 # Description : Ce script génère un fichier de métadonnées pour un jeu de données puis, en utilisant
 # l'API bloxberg, il certifie ce fichier de métadonnées sur la blockchain bloxberg
 # et télécharge le certificat correspondant.
-# Usage : ./metadata-bloxberg-certify.sh BLOXBERG_API_KEY BLOXBERG_WALLET_ADDRESS
+# Usage : ./metadata-bloxberg-certify.sh CLÉ_API_BLOXBERG WALLET_BLOXBERG JEU_DE_DONNÉES
 # Référence API bloxberg : https://certify.bloxberg.org/docs
 # Licence : GPL-3+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Auteur : Cédric Goby
 # Versioning : https://forgemia.inra.fr/blockchain-esr/bloxberg-certify-metadata
 
-# Vérifier si la clef API et l'adresse du wallet bloxberg ont été fournies comme paramètres positionnels
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Vous n'avez pas fourni votre clé API bloxberg et/ou l'adresse de votre wallet bloxberg"
-  echo "Usage : ./metadata-bloxberg-certify.sh BLOXBERG_API_KEY BLOXBERG_WALLET_ADDRESS"
+# Vérifier si la clef API, l'adresse du wallet bloxberg et le chemin vers le jeu de données
+# ont été fournis au script.
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo "Vous n'avez pas fourni votre clé API bloxberg, l'adresse de votre wallet bloxberg"
+  echo "ou le chemin vers votre jeu de données"
+  echo "Usage : ./metadata-bloxberg-certify.sh CLÉ_API_BLOXBERG WALLET_BLOXBERG JEU_DE_DONNÉES"
   exit 1  # Quitter le script avec un code d'erreur
 fi
 
 # --------------------------------------------------------------------------------------------
-# Génération du fichier de métadonnées "metadata.json" pour un jeu de données.
+# ETAPE 1
 # --------------------------------------------------------------------------------------------
-# Chemin du répertoire contenant le jeu de données.
-# *** Dans l'exemple ci-dessous il s'agit d'un répertoire local contenant des newsletters mensuelles sur la sécurité informatique au format MARKDOWN.
-# *** Chaque mois la nouvelle newsletter est envoyée (push) sur Gitlab : https://gitlab.com/CedricGoby/newsletter-securite-informatique/ 
-# *** ainsi que le nouveau fichier "metadata.json" incluant cette nouvelle newsletter (un nouveau fichier MARDOWN).
-_dataset_folder=/home/cedric/Documents/DEV/scripts-rss/newsletter-securite-informatique
-# Nom du fichier de métadonnées.
-_metadata_file=metadata.json
-# Réinitialisation du fichier.
->$_metadata_file
+# Création du fichier de métadonnées "metadata.json" pour un jeu de données. 
+# Le fichier "metadata.json" est au format JSON-LD. Les métadonnées sont décrites en utilisant les standards DUBLIN CORE et PREMIS.
+# --------------------------------------------------------------------------------------------
 
-# Insertion du début du fichier de métadonnées.
-# *** Utilisation du standard DUBLIN CORE (https://www.dublincore.org/specifications/dublin-core/dcmi-terms/),
-# *** du standard PREMIS (https://www.loc.gov/standards/premis/)
-# *** et du format JSON-LD (https://www.w3.org/TR/json-ld11/) pour décrire le jeu de données.
-cat >$_metadata_file << EOF
+# Début du fichier "metadata.json".
+cat >metadata.json << EOF
 {
   "@context": {
     "dc": "http://purl.org/dc/terms/",
@@ -45,8 +38,8 @@ cat >$_metadata_file << EOF
   "dc:format": "text/markdown",
   "dc:title": "Revue de presse sur la sécurité informatique",
   "dc:subject": "http://id.loc.gov/authorities/subjects/sh90001862",
-  "dc:date": "$(date -u "+%Y-%m-%dT%H:%M:%S.%NZ" | sed 's/.\{6\}$//' | sed 's/$/Z/')",
-  "dc:description": "Cette revue de presse généraliste et mensuelle a pour but de sensibiliser le public aux problématiques de la sécurité informatique.",
+  "dc:date": "$(date -u +"%Y-%m-%dT%H:%M:%S.%6N")",
+  "dc:description": "Cette revue de presse généraliste mensuelle a pour but de sensibiliser le public aux problématiques de la sécurité informatique.",
   "dc:publisher": [
     "http://viaf.org/viaf/3881157583857733970006",
     "http://viaf.org/viaf/314926296"
@@ -54,11 +47,8 @@ cat >$_metadata_file << EOF
   "dc:rights":"http://spdx.org/licenses/CC-BY-NC-SA-4.0.json",
   "dc:identifier": [
 EOF
-
-# On classe les fichier MARKDOWN par date pour plus de clarté.
-for _file in $(ls -t $_dataset_folder/*.md); do
-# On insert l'URI et la SOMME DE CONTRÔLE de chaque fichier MARKDOWN (.md) dans le fichier de métadonnées "metadata.json".
-# *** Dans notre exemple l'URI correspond à l'URL Gitlab de chaque fichier MARKDOWN (URL pérennes tant que le dépôt existe).
+# On insert l'URI pérenne et la SOMME DE CONTRÔLE de chaque fichier markdown dans le fichier "metadata.json".
+for _file in $(ls -t $3/*.md); do
 cat >>metadata.json << EOF
     {
       "@value": "https://gitlab.com/CedricGoby/newsletter-securite-informatique/-/blob/master/$(basename $_file)",
@@ -71,28 +61,30 @@ cat >>metadata.json << EOF
     },
 EOF
 done
-# Suppression de la dernière ligne du fichier pour retirer "},"
-sed -i '$d' "$_metadata_file"
-# Clotûre du fichier de métadonnées.
-cat >>$_metadata_file << EOF
+# Suppression de la dernière ligne du fichier "metadata.json" pour retirer "},"
+sed -i '$d' metadata.json
+# Fin du fichier "metadata.json".
+cat >>metadata.json << EOF
      }
    ]
  }
 EOF
-
 # TODO : Générer un fichier README.txt (lisible par les humains) à partir du fichier de métadonnées JSON.
 
 # --------------------------------------------------------------------------------------------
-# Écriture dans un fichier TEMPORAIRE de la valeur JSON pour la clé "metadataJson" de l'API bloxberg.
+# ETAPE 2
 # --------------------------------------------------------------------------------------------
-# Nom du fichier TEMPORAIRE contenant la valeur pour la clé "metadataJson".
-_metadataJson="bloxberg_metadataJson.json"
-# Création du fichier TEMPORAIRE
-touch $_metadataJson
+# Certification du fichier "metadata.json" dans la blockchain bloxberg en utilisant l'API bloxberg.
+# --------------------------------------------------------------------------------------------
 
-# Écriture de la valeur JSON pour la clé "metadataJson" dans le fichier TEMPORAIRE en utilisant le DUBLIN CORE.
-# *** Dans notre exemple "dc:identifier" correspond à l'URL Gitlab du fichier "metadata.json" (URL pérenne tant que le dépôt existe):
-# *** https://gitlab.com/CedricGoby/newsletter-securite-informatique/-/blob/master/metadata.json
+# --------------------------------------------------------------------------------------------
+# Création d'un fichier contenant la valeur de la clé "metadataJson" attendue par l'API bloxberg.
+# La valeur est au format JSON et utilise le standard Dublin Core.
+# --------------------------------------------------------------------------------------------
+# Nom du fichier contenant la valeur de la clé "metadataJson".
+_metadataJson="bloxberg_metadataJson.json"
+
+# Création du fichier.
 cat >$_metadataJson << EOF
 {
   "@context": {
@@ -119,7 +111,7 @@ cat >$_metadataJson << EOF
 EOF
 
 # --------------------------------------------------------------------------------------------
-# Appel à l'API bloxberg pour certifier le fichier de métadonnées "metadata.json".
+# Appel à l'API bloxberg pour certifier le fichier de métadonnées "metadata.json" sur la blockchain bloxberg.
 # --------------------------------------------------------------------------------------------
 # Clé API fournie par bloxberg (Paramètre positionnel 1).
 _api_key="$1"
@@ -127,17 +119,19 @@ _api_key="$1"
 _public_key="$2"
 # SOMME DE CONTRÔLE du fichier de métadonnées "metadata.json" (crid = Content Reference IDentifier).
 _crid=$(sha256sum "$_metadata_file" | awk '{print $1}')
-# A partir du fichier TEMPORAIRE on formate la valeur de la clé "metadataJson" pour l'API.
+# A partir du fichier contenant la valeur de la clé "metadataJson" on formate la valeur de la clé pour l'API.
 _metadataJson=$(cat $_metadataJson | jq -c . | sed -e 's/"/\\"/g')
-# Nom du fichier TEMPORAIRE contenant la réponse JSON de l'API.
+# Nom du fichier contenant la réponse JSON de l'API.
 _json_response=bloxberg_json_response.json
-## Création du fichier TEMPORAIRE
+## Création du fichier contenant la réponse JSON de l'API.
 touch $_json_response
 # Nom du fichier de logs (Erreurs).
 _error_log=error.log
 
-# Envoi de la requête CURL, écriture de la réponse JSON de l'API dans un fichier,
-# récupération du code de réponse HTTP.
+# --------------------------------------------------------------------------------------------
+# Envoi de la requête CURL, écriture de la réponse JSON de l'API dans un fichier
+# et récupération du code de réponse HTTP.
+# --------------------------------------------------------------------------------------------
 _http_request=$(curl --write-out '%{http_code}' -X 'POST' \
   'https://certify.bloxberg.org/createBloxbergCertificate' \
   -H 'accept: application/json' \
@@ -153,13 +147,17 @@ _http_request=$(curl --write-out '%{http_code}' -X 'POST' \
   "metadataJson": "'"$_metadataJson"'"
 }' --output $_json_response --silent)
 
-# Vérification du code de retour HTTP (Code 200 attendu).
+# --------------------------------------------------------------------------------------------
+# Vérification du code de réponse HTTP (Code 200 attendu).
+# --------------------------------------------------------------------------------------------
 if [ ${_http_request} -ne 200 ] ; then
     echo "$(date) - HTTP error : $_http_request" | tee -a $_error_log
-    exit 1
+    exit 1  # Quitter le script avec un code d'erreur
 fi
 
+# --------------------------------------------------------------------------------------------
 # Vérification de la validité de la réponse JSON.
+# --------------------------------------------------------------------------------------------
 # Début de la séquence JSON attendue.
 _expected_json_response='[{"@context":["https://www.w3.org/2018/credentials/v1",'
 # Formatage de la réponse JSON sur une ligne.
@@ -168,21 +166,23 @@ _one_line_json_response=$(jq -c . $_json_response)
 # dans le fichier de log.
 if [[ ! "$_one_line_json_response" =~ ^"$_expected_json_response" ]]; then
     echo "$(date) - JSON error : $(cat $_json_response)" | tee -a $_error_log
-    exit 1
+    exit 1  # Quitter le script avec un code d'erreur
 fi
 
 # FIXME : Délai de validation du bloc ?
 sleep 900
 
 # --------------------------------------------------------------------------------------------
-# Appel à l'API bloxberg pour générer le certificat au format PDF et le télécharger en tant
-# qu'archive ZIP.
+# ETAPE 3
 # --------------------------------------------------------------------------------------------
-# Nom du fichier ZIP contenant le certificat au format PDF.
+# Appel à l'API bloxberg pour télécharger le certificat du fichier "metadata.json" au format PDF.
+# --------------------------------------------------------------------------------------------
+# Nom du fichier ZIP téléchargé contenant le certificat au format PDF.
 _bloxberg_certificate=bloxberg_certificate-crid-$_crid.zip
 
-# Envoi de la requête CURL, téléchargement de l'archive ZIP,
-# récupération du code de réponse HTTP.
+# --------------------------------------------------------------------------------------------
+# Envoi de la requête CURL, téléchargement du fichier ZIP et récupération du code de réponse HTTP.
+# --------------------------------------------------------------------------------------------
 _http_request=$(curl --write-out '%{http_code}' -X 'POST' \
   'https://certify.bloxberg.org/generatePDF' \
   -H 'accept: application/json' \
@@ -190,7 +190,9 @@ _http_request=$(curl --write-out '%{http_code}' -X 'POST' \
   -H 'Content-Type: application/json' \
   -d ''"$(cat $_json_response)"'' --output $_bloxberg_certificate --silent)
 
-# Vérification du code de retour HTTP (Code 200 attendu).
+# --------------------------------------------------------------------------------------------
+# Vérification du code de réponse HTTP (Code 200 attendu).
+# --------------------------------------------------------------------------------------------
 if [ ${_http_request} -ne 200 ] ; then
     echo "$(date) - HTTP error : $_http_request" | tee -a $_error_log
     exit 1
@@ -199,15 +201,6 @@ fi
 # TODO : Vérifier l'intégrité du fichier téléchargé.
 # TODO : Extraire l'archive.
 
-# Suppression du fichier TEMPORAIRE contenant la valeur pour la clé "metadataJson".
-#rm $_metadataJson
-
-# Suppression du fichier TEMPORAIRE  contenant la réponse JSON de l'API.
-#rm $_json_response
-
-# Déplacement du fichier "metadata.json" vers le répertoire contenant le jeu de données (en écrasant le fichier "metadata.json" existant).
-# *** Dans notre exemple on déplace le fichier "metadata.json" dans le répertoire local /home/cedric/newsletter-securite-informatique
-# *** contenant les newsletters mensuelles sur la sécurité informatique.
 #mv -f $_metadataJson $_dataset_folder/$_metadataJson
 
 # Mise à jour du dépôt Gitlab avec le nouveau fichier "metadata.json"
